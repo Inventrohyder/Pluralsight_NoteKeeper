150cf34c119e35f7b6efb633c5e8fc49cf4d1339
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-26 22:34:34 +0300

    test: NavigationView and RecyclerView note tests

--- app/src/androidTest/java/com/inventrohyder/pluralsight_notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/com/inventrohyder/pluralsight_notekeeper/NextThroughNotesTest.java
@@ -1,12 +1,16 @@
 package com.inventrohyder.pluralsight_notekeeper;
 
 import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
@@ -27,5 +31,23 @@ public class NextThroughNotesTest {
     public void NextThroughNotes() {
         // Using the DrawerActions to perform actions on the DrawerLayout
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+
+        // Select the notes NavigationView in the draw to select the notes
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        // Select the first view from the RecyclerView and click it to open the NoteActivity
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // Gain access to the first Note within our data
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        int index = 0;
+        NoteInfo note = notes.get(index);
+
+        // Check if the selected note within our Data matches the fields in the opened NoteActivity
+        onView(withId(R.id.spinner_courses)).check(
+                matches(withSpinnerText(note.getCourse().getTitle()))
+        );
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
     }
 }
