2c206600228678466febbc067b0b10632f0e4af2
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-27 20:53:53 +0300

    feat: reflect preference choice in NavigationView

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/MainActivity.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/MainActivity.java
@@ -1,10 +1,12 @@
 package com.inventrohyder.pluralsight_notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -121,6 +123,24 @@ public class MainActivity extends AppCompatActivity
     protected void onResume() {
         super.onResume();
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
+
+    private void updateNavHeader() {
+        // Get Access to the NavigationView TextView(s)
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textUserName = headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAddress = headerView.findViewById(R.id.text_email_address);
+
+        // Access the set preferences
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = pref.getString("user_display_name", "");
+        String email_Address = pref.getString("user_email_address", "");
+
+        // Update the NavigationView TextView(s) with the appropriate preference values
+        textUserName.setText(userName);
+        textEmailAddress.setText(email_Address);
     }
 
     private void initializeDisplayContent() {
