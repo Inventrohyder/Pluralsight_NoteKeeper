9241307abe035071877b8766adf262449cc114ad
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-28 14:17:19 +0300

    feat: implement BaseColumns to add an integer primary key
    An integer primary key ensures that there is a quick access to the rows of data.
    It is internally efficient for SQLite and the Android Framework to access data
    in this manner.

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperDatabaseContract.java
@@ -1,13 +1,17 @@
 package com.inventrohyder.pluralsight_notekeeper;
 
+import android.provider.BaseColumns;
+
 public final class NoteKeeperDatabaseContract {
 
     // Constraints
     static final String NOT_NULL = " NOT_NULL ";
     static final String UNIQUE = " UNIQUE ";
+    static final String PRIMARY_KEY = " PRIMARY KEY ";
 
     // Storage classes
     static final String TEXT = " TEXT ";
+    static final String INTEGER = " INTEGER ";
 
     private NoteKeeperDatabaseContract() {
     }  // Make non-creatable
@@ -15,7 +19,7 @@ public final class NoteKeeperDatabaseContract {
 
     // Classes for the Tables
     // course_info table
-    public static final class CourseInfoEntry {
+    public static final class CourseInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
@@ -23,12 +27,13 @@ public final class NoteKeeperDatabaseContract {
         // CREATE TABLE course_info (course_id, course_title)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + INTEGER + PRIMARY_KEY + ", " +
                         COLUMN_COURSE_ID + TEXT + UNIQUE + NOT_NULL + ", " +
                         COLUMN_COURSE_TITLE + TEXT + NOT_NULL + ")";
     }
 
     // note_info table
-    public static final class NoteInfoEntry {
+    public static final class NoteInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
@@ -37,6 +42,7 @@ public final class NoteKeeperDatabaseContract {
         // CREATE TABLE note_info (note_id, note_title, note_text, course_id)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + INTEGER + PRIMARY_KEY + ", " +
                         COLUMN_NOTE_TITLE + TEXT + NOT_NULL + ", " +
                         COLUMN_NOTE_TEXT + TEXT + ", " +
                         COLUMN_COURSE_ID + TEXT + NOT_NULL + ")";
