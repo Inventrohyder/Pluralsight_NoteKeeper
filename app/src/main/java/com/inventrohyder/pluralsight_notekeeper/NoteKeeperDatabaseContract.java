96d1a02dc8fbfb93121a3457ec1e2c3bc22dbf18
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-28 14:08:06 +0300

    feat: apply table constraints and class names

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperDatabaseContract.java
@@ -1,6 +1,14 @@
 package com.inventrohyder.pluralsight_notekeeper;
 
 public final class NoteKeeperDatabaseContract {
+
+    // Constraints
+    static final String NOT_NULL = " NOT_NULL ";
+    static final String UNIQUE = " UNIQUE ";
+
+    // Storage classes
+    static final String TEXT = " TEXT ";
+
     private NoteKeeperDatabaseContract() {
     }  // Make non-creatable
 
@@ -10,19 +18,18 @@ public final class NoteKeeperDatabaseContract {
     public static final class CourseInfoEntry {
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
-        public static final String COLUMN_COURSE_TITILE = "course_title";
+        public static final String COLUMN_COURSE_TITLE = "course_title";
 
         // CREATE TABLE course_info (course_id, course_title)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_COURSE_ID + ", " +
-                        COLUMN_COURSE_TITILE + ")";
+                        COLUMN_COURSE_ID + TEXT + UNIQUE + NOT_NULL + ", " +
+                        COLUMN_COURSE_TITLE + TEXT + NOT_NULL + ")";
     }
 
     // note_info table
     public static final class NoteInfoEntry {
         public static final String TABLE_NAME = "note_info";
-        public static final String COLUMN_NOTE_ID = "note_id";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
         public static final String COLUMN_COURSE_ID = "course_id";
@@ -30,9 +37,8 @@ public final class NoteKeeperDatabaseContract {
         // CREATE TABLE note_info (note_id, note_title, note_text, course_id)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_NOTE_ID + ", " +
-                        COLUMN_NOTE_TITLE + ", " +
-                        COLUMN_NOTE_TEXT + ", " +
-                        COLUMN_COURSE_ID + ")";
+                        COLUMN_NOTE_TITLE + TEXT + NOT_NULL + ", " +
+                        COLUMN_NOTE_TEXT + TEXT + ", " +
+                        COLUMN_COURSE_ID + TEXT + NOT_NULL + ")";
     }
 }
