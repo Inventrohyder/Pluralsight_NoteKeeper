353e3032c9406df2373eb5e60b70395562dc5f43
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-29 14:19:27 +0300

    feat: set the EditTextPreference inputType to email address

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/SettingsActivity.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.inventrohyder.pluralsight_notekeeper;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -26,6 +30,25 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            EditTextPreference editTextPreference = getPreferenceManager().findPreference("user_email_address");
+            assert editTextPreference != null;
+            editTextPreference.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                }
+            });
+
+//            androidx.preference.EditTextPreference editTextPreference = getPreferenceManager().findPreference("use_key_from_editTextPreference_in_xml_file");
+//            editTextPreference.setOnBindEditTextListener(new androidx.preference.EditTextPreference.OnBindEditTextListener() {
+//                @Override
+//                public void onBindEditText(@NonNull EditText editText) {
+//                    editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+//
+//                }
+//            });
+
         }
     }
 }
