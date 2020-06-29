52dd3ecd552944e7f4bf884fdf2e39c2960d70d3
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-29 13:56:43 +0300

    feat: load notes from cursor into MainActivity

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/MainActivity.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/MainActivity.java
@@ -2,6 +2,8 @@ package com.inventrohyder.pluralsight_notekeeper;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +23,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.inventrohyder.pluralsight_notekeeper.NoteKeeperDatabaseContract.NoteInfoEntry;
 
 import java.util.List;
 
@@ -145,8 +148,26 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-        mNoteRecyclerAdapter.notifyDataSetChanged();
+        loadNotes();
         updateNavHeader();
+    }
+
+    private void loadNotes() {
+        SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
+
+        // Query the notes
+        String[] noteColumns = {
+                NoteInfoEntry.COLUMN_NOTE_TITLE,
+                NoteInfoEntry.COLUMN_COURSE_ID,
+                NoteInfoEntry._ID
+        };
+
+        // Load the notes ordered by both the courseId and NoteTitle
+        String noteOrderBy = NoteInfoEntry.COLUMN_COURSE_ID + ", " + NoteInfoEntry.COLUMN_NOTE_TITLE;
+        final Cursor noteCursor = db.query(NoteInfoEntry.TABLE_NAME, noteColumns,
+                null, null, null, null, noteOrderBy);
+
+        mNoteRecyclerAdapter.changeCursor(noteCursor);
     }
 
     private void updateNavHeader() {
@@ -174,8 +195,7 @@ public class MainActivity extends AppCompatActivity
         mCourseLayoutManager = new GridLayoutManager(this,
                 getResources().getInteger(R.integer.course_grid_span));
 
-        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+        mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, null);
 
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
         mCourseRecyclerAdapter = new CourseRecyclerAdapter(this, courses);
--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteRecyclerAdapter.java
@@ -38,7 +38,7 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
         mIdPos = mCursor.getColumnIndex(NoteInfoEntry._ID);
     }
 
-    private void changeCursor(Cursor cursor) {
+    public void changeCursor(Cursor cursor) {
         if (mCursor != null)
             mCursor.close();
         mCursor = cursor;
