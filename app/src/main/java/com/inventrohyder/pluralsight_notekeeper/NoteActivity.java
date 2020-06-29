d848358f61d8b3d7de3880537c422d197be1ab69
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-06-29 12:47:03 +0300

    feat: populating the spinner using database data

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteActivity.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteActivity.java
@@ -71,6 +71,8 @@ public class NoteActivity extends AppCompatActivity {
         mAdapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinnerCourses.setAdapter(mAdapterCourses);
 
+        loadCourseData();
+
         readDisplayStateValues();
         if (!mIsNewNote)
             lodeNoteData();
@@ -82,6 +84,20 @@ public class NoteActivity extends AppCompatActivity {
 
         Log.d(tag, "onCreate");
 
+    }
+
+    private void loadCourseData() {
+        SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
+        String[] courseColumns = {
+                CourseInfoEntry.COLUMN_COURSE_TITLE,
+                CourseInfoEntry.COLUMN_COURSE_ID,
+                CourseInfoEntry._ID
+        };
+
+        Cursor cursor = db.query(CourseInfoEntry.TABLE_NAME, courseColumns,
+                null, null, null, null, CourseInfoEntry.COLUMN_COURSE_TITLE);
+
+        mAdapterCourses.changeCursor(cursor);
     }
 
     @Override
