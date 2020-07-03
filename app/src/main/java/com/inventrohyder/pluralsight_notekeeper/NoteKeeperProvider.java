4f5fb9a59ad65a703bce205531634d705bd58194
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-03 04:00:29 +0300

    feat: implement a database join query

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperProvider.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperProvider.java
@@ -22,9 +22,12 @@ public class NoteKeeperProvider extends ContentProvider {
     public static final int NOTES = 1;
     private static UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
+    public static final int NOTES_EXPANDED = 2;
+
     static {
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Courses.PATH, COURSES);
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH, NOTES);
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH_EXPANDED, NOTES_EXPANDED);
     }
 
     public NoteKeeperProvider() {
@@ -71,9 +74,23 @@ public class NoteKeeperProvider extends ContentProvider {
                 cursor = db.query(NoteInfoEntry.TABLE_NAME, projection, selection, selectionArgs,
                         null, null, sortOrder);
                 break;
+            case NOTES_EXPANDED:
+                cursor = notesExpandedQuery(db, projection, selection, selectionArgs, sortOrder);
+                break;
         }
 
         return cursor;
+    }
+
+    private Cursor notesExpandedQuery(SQLiteDatabase db, String[] projection, String selection,
+                                      String[] selectionArgs, String sortOrder) {
+        String tablesWithJoin = NoteInfoEntry.TABLE_NAME + " JOIN " +
+                CourseInfoEntry.TABLE_NAME + " ON " +
+                NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID) + " = " +
+                CourseInfoEntry.getQName(CourseInfoEntry.COLUMN_COURSE_ID);
+
+        return db.query(tablesWithJoin, projection, selection, selectionArgs,
+                null, null, sortOrder);
     }
 
     @Override
