6450eb3ea7501ac4cf03375aed747e5db7df3b7c
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-03 03:32:53 +0300

    feat: add the table columns to the content provider contract

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperProviderContract.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteKeeperProviderContract.java
@@ -1,6 +1,7 @@
 package com.inventrohyder.pluralsight_notekeeper;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 final class NoteKeeperProviderContract {
     public static final String AUTHORITY = "com.inventrohyder.pluralsight_notekeeper.provider";
@@ -9,13 +10,26 @@ final class NoteKeeperProviderContract {
     private NoteKeeperProviderContract() {
     }
 
-    public static final class Courses {
+    protected interface CoursesIdColumns {
+        String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CourseColumns {
+        String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected interface NotesColumns {
+        String COLUMN_NOTE_TITLE = "note_title";
+        String COLUMN_NOTE_TEXT = "note_text";
+    }
+
+    public static final class Courses implements CourseColumns, BaseColumns, CoursesIdColumns {
         public static final String PATH = "courses";
         // content://com.inventrohyder.pluralsight_notekeeper.provider/courses
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
 
-    public static final class Notes {
+    public static final class Notes implements NotesColumns, BaseColumns, CoursesIdColumns {
         public static final String PATH = "notes";
         // content://com.inventrohyder.pluralsight_notekeeper.provider/notes
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
