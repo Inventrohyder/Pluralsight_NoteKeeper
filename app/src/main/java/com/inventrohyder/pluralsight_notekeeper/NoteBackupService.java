d44fef7b153ba83c517b83a6d3a668238d930f48
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-06 14:31:32 +0300

    feat: create a backup service

---
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteBackupService.java
@@ -0,0 +1,26 @@
+package com.inventrohyder.pluralsight_notekeeper;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
+public class NoteBackupService extends IntentService {
+
+    public static final String EXTRA_COURSE_ID = "com.inventrohyder.pluralsight_notekeeper.extra.COURSE_ID";
+
+    public NoteBackupService() {
+        super("NoteBackupService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            String backupCourseId = intent.getStringExtra(EXTRA_COURSE_ID);
+            NoteBackup.doBackup(this, backupCourseId);
+        }
+    }
+}
