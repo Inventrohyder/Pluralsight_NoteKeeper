4b43baa914212b1ba5dd1eeaaf0f2c17331418c5
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-07 11:11:48 +0300

    feat: launch a job work in the background

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteUploaderJobService.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteUploaderJobService.java
@@ -2,16 +2,35 @@ package com.inventrohyder.pluralsight_notekeeper;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.net.Uri;
+import android.os.AsyncTask;
 
 public class NoteUploaderJobService extends JobService {
 
     public static final String EXTRA_DATA_URI = "com.inventrohyder.pluralsight_notekeeper.DATA_URI";
+    private NoteUploader mNoteUploader;
 
     public NoteUploaderJobService() {
     }
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
+
+        AsyncTask<JobParameters, Void, Void> task = new AsyncTask<JobParameters, Void, Void>() {
+            @Override
+            protected Void doInBackground(JobParameters... backgroundParams) {
+                JobParameters jobParams = backgroundParams[0];
+
+                String stringDataUri = jobParams.getExtras().getString(EXTRA_DATA_URI);
+                Uri dataUri = Uri.parse(stringDataUri);
+                mNoteUploader.doUpload(dataUri);
+                return null;
+            }
+        };
+
+        mNoteUploader = new NoteUploader(this);
+
+
         return false;
     }
 
